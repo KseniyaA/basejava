df99d5c06adcf2273be493afd01fde469b9b974e
Ksu <akushkina.ka@gmail.com>
2017-10-27 22:32:37 +0700

    - Добавлен метод update; - В методы update/delete/get/save добавлены проверки через метод getIndexInStorageByUuid(...); - Рефакторинг с целью исключить дублирование кода;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,7 +14,35 @@ public class ArrayStorage {
         factNumber = 0;
     }
 
+    /**
+     * Получение индекса резюме в массиве
+     * @param uuid идентификатор резюме
+     * @return -1 - если резюме не найдено в массиве, >= 0 - индекс резюме в массиве storage
+     */
+    private int getIndexInStorageByUuid(String uuid) {
+        for(int i = 0; i < factNumber; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void update (Resume r) {
+        int indexInStorageByUuid = getIndexInStorageByUuid(r.uuid);
+        if (indexInStorageByUuid == -1) {
+            System.out.println(String.format("Резюме с id = [%s] не найдено в массивею. Обновление невозможно.", r.uuid));
+            return;
+        }
+        storage[indexInStorageByUuid] = r;
+    }
+
     void save(Resume r) {
+        int indexInStorageByUuid = getIndexInStorageByUuid(r.uuid);
+        if (indexInStorageByUuid > -1) {
+            System.out.println(String.format("Резюме с id = [%s] уже сохранено в базе. Сохранение не возможно.", r.uuid));
+            return;
+        }
         if (factNumber == 10000) {
             System.out.println("Невозможно добавить элемент, т.к. массив заполнен.");
             return;
@@ -24,22 +52,21 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for(int i = 0; i < factNumber; i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                return storage[i];
-            }
+        int indexInStorageByUuid = getIndexInStorageByUuid(uuid);
+        if (indexInStorageByUuid == -1) {
+            return null;
         }
-        return null;
+        return storage[indexInStorageByUuid];
     }
 
     void delete(String uuid) {
-        for(int i = 0; i < factNumber; i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                storage[i] = storage[factNumber-1];
-                storage[factNumber-1] = null;
-                break;
-            }
+        int indexInStorageByUuid = getIndexInStorageByUuid(uuid);
+        if (indexInStorageByUuid == -1) {
+            System.out.println(String.format("Резюме с id = [%s] не хранится в storage.", uuid));
+            return;
         }
+        storage[indexInStorageByUuid] = storage[factNumber-1];
+        storage[factNumber-1] = null;
         factNumber--;
         // не нужно сохранять порядок элементов, поэтому закоментировала стркоу с сортировкой
         // Arrays.sort(storage, Comparator.<Resume>nullsLast(Comparator.naturalOrder()));

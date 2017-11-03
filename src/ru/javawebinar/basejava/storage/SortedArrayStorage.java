ec3d58227265a84b7d144c82405832d94de0d6e4
Ksu <akushkina.ka@gmail.com>
2017-11-03 22:09:10 +0700

    HW3 1. Метод clear и getAll перенесены в абстрактный класс; 2. В SortedArrayStorage ркализованы методы update, save, delete;

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -5,29 +5,44 @@ import ru.javawebinar.basejava.model.Resume;
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage{
-    @Override
-    public void clear() {
-
-    }
 
     @Override
     public void update(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+            Arrays.sort(storage);
+        }
     }
 
     @Override
     public void save(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            System.out.println(String.format("Resume with uid = [%s] already exist", r.getUuid()));
+            return;
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        }
+        int indexForInsert = -index - 1;
+        // Копируем часть элементов массива, от индекса indexForInsert, в этот же массив, начиная с indexForInsert + 1
+        System.arraycopy(storage, indexForInsert, storage, indexForInsert + 1, size - indexForInsert);
+        storage[indexForInsert] = r;
+        size++;
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println(String.format("Resume with uid = [%s] does not exist", uuid));
+            return;
+        }
+        System.arraycopy(storage, index+1, storage, index , size - index);
+        storage[size] = null;
+        size--;
     }
 
     @Override
